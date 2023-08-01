4e6a3c13a15514cee3ada38f3ff184decfebadf0
Siri Ganapathineedi <snganapat@gmail.com>
2023-08-01 16:16:13 -0500

    Initial Commit

---
+++ FinalProjectMapPage/FinalProjectMapPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectMapPageApp.swift
+//  FinalProjectMapPage
+//
+//  Created by Siri Ganapathineedi on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectMapPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
