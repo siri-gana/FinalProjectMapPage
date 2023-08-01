1d8e1bf083727c93908c92517a177067540a707b
Siri Ganapathineedi <snganapat@gmail.com>
2023-08-01 16:45:55 -0500

    Created a general layout of the "Opportunity Map" page with temporary colors and information filled in.

--- FinalProjectMapPage/ContentView.swift
+++ FinalProjectMapPage/ContentView.swift
@@ -8,19 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var volunteering1 = "RedCross Volunteering"
+    @State private var volunteering2 = "Medical Justice Alliance"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.brown) //will need to change color later to hexcode #E9E9E9 prediced by group members
+                .ignoresSafeArea()
+            VStack {
+                Text("Opportunity Map") //will need to change text to hexcode #939FA7
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                VStack {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Placeholder")/*@END_MENU_TOKEN@*///insert map here
+                }
+                .padding()
+                .background(.white)
+                .cornerRadius(15)
+                .padding()
+                .font(.footnote)
+                Text("Opportunity List") //will need to change text to hexcode #939FA7
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                Text(volunteering1)
+                Button("random fact"){
+                    volunteering1 = "my favorite show of ALL time is avatar the last airbender!!"
+                    
+                }
+            }
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
